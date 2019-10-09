80df72c9c93a24a3c46f091cc4b506172d6d7d53
Preston Stone <Josephstone@Prestons-MacBook-Pro.local>
2019-10-09 12:10:36 -0600

    sign in test

--- pageObjects/carObjects.js
+++ pageObjects/carObjects.js
@@ -2,21 +2,33 @@ var commands = {
     signIn: function (profile) {
         this
             .click('@signIn')
+            .waitForElementVisible('@signIn2')
             .click('@signIn2')
-            .clearValue(profile)
+            .waitForElementVisible('@userI')
+            .clearValue('@userI')
+            .setValue('@userI', profile.user)
+            .clearValue('@passI')
+            .setValue('@passI', profile.pass)
+            .click('[value="Login"]')
+            .pause(3000)
         return this
     }
 }
 
 module.exports = {
     url: 'https://www.carsforsale.com',
-    commands: 'commands',
+    commands: [commands],
     elements: {
         signIn: '[id="signInIcon"]',
         favorites: '[title="Favorites"]',
         make: '[name="make"]',
         maxprice: '[name="maxprice"]',
         search: '[class="btn btn-lg btn-block btn-primary shadow-style search-banner-button"]',
-        signIn2: '(//a[@href="/profile"])[2]'
+        signIn2: { selector: '(//a[@href="/profile"])[2]', locateStrategy: 'xpath' },
+        userI: '[id="profileLoginEmail"]',
+        passI: '[id="profileLoginPassword"]',
+        user:'tjones@msn.com',
+        pass: 'QApasstest',
+
     }
 }
--- test/tes.js
+++ test/tes.js
@@ -1,16 +1,19 @@
-var cars
-module.exports = {
-    beforeEach: browser => {
-        cars = browser.page.carObjects()
-        cars.navigave()
-    },
-    after: browser=>{
-        cars.end()
-    },
-    'sign in' :browser =>{
-        cars
-        //clicking on the sign in button and signing in using the given info.
-            .signIn({user: 'tjones@msn.com',
-            pass:'QApasstest'})
+var cars =
+    module.exports = {
+        beforeEach: browser => {
+            cars = browser.page.carObjects()
+            cars.navigate()
+        },
+        after: browser => {
+            browser.end()
+        },
+        'sign in': browser => {
+            //clicking on the sign in button and signing in using the given info.
+            var data = {
+                user:'tjones@msn.com',
+                pass:'QApasstest'
+            }
+            cars.signIn(data)
+            .expect.element('@signIn').to.contain.text('Tom J')
+        }
     }
-}
